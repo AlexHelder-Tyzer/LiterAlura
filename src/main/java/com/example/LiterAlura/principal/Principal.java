bfc3afec176774e9c157f7456d2aa31f69809a82
tyzer <helder.huancara@gmail.com>
2024-06-30 20:54:37 -0500

    Creacion de menu y config

--- src/main/java/com/example/LiterAlura/principal/Principal.java
+++ src/main/java/com/example/LiterAlura/principal/Principal.java
@@ -1,2 +1,40 @@
-package com.example.LiterAlura.principal;public class Principal {
+package com.example.LiterAlura.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+
+    public void runProgram() {
+        var opcion = -1;
+
+        while (opcion != 0) {
+            var menu = """
+                    ====== Bienvenidos al Sistema LiterAlura ======
+                    1. Buscar libro por titulo
+                    2. Listar libros registrados
+                    3. Listar autores regitrados
+                    4. Listar autores vivos en un determinado anho
+                    5. Listar libros por idioma
+
+                    0. Salir
+                    """;
+            System.out.println(menu);
+            System.out.println("============ Ingrese la opcion: ");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("test");
+                    ;
+                    break;
+                case 0:
+                    System.out.println("Cerrando la palicacion!!");
+                    break;
+                default:
+                    System.out.println("Opcion invalida!!");
+            }
+        }
+    }
 }
